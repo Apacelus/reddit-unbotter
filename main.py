8f57ae9fd37b72414264ae0c86c8f7a0bea2b674
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-07-11 14:57:52 +0200

    starting rewrite of the code to implement selenium

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 from ast import literal_eval
 from random import randint
 from time import sleep
+from hashlib import md5
+import json
 
 user_agents_amount = 0
 
@@ -38,7 +40,7 @@ def login_users(mode="force"):
             pass
 
 
-def initialize():
+def initialize_old():
     logging.info("\n\nNew log:")
     logging.info("Initializing")
     try:
@@ -54,7 +56,56 @@ def initialize():
     logging.info("Available user agents: " + str(user_agents_amount))
 
 
+def add_accounts():
+    logging.info("Comparing accounts")
+    # read accounts.txt and parse it
+    with open("accounts.txt", "r") as f:
+        accounts_file = f.readlines()
+        del accounts_file[0]
+        del accounts_file[0]
+        for x in range(len(accounts_file)):
+            try:
+                accounts_file.remove("")
+            except ValueError:
+                pass
+            accounts_file[x] = accounts_file[x].replace("\n", "")
+    with open('data.json', 'r') as f:
+        data_json = json.load(f)
+    # check if username and passwd match and exist
+    to_be_added = []
+    for x in range(len(accounts_file)):
+        try:
+            if not data_json[accounts_file[x][:accounts_file[x].find(":")]]["password"] == accounts_file[x][
+                                                                                           accounts_file[x].find(
+                                                                                               ":") + 1:]:
+                to_be_added.append(accounts_file[x])
+        except KeyError:
+            to_be_added.append(accounts_file[x])
+    logging.info("accounts to be added: " + str(to_be_added))
+
+
+def initialize():
+    logging.info("\n\nNew log:")
+    logging.info("Initializing")
+    with open("accounts.txt", "r") as f:
+        accounts_amount = len(f.readline()) - 2
+    if accounts_amount <= 0:
+        logging.error("No accounts found, exiting")
+        print('No accounts found! Please fill in at least one account in "accounts.txt"')
+        exit(1)
+    else:
+        # Read the settings.json file
+        with open('settings.json', 'r') as json_file:
+            settings_json = json.load(json_file)
+        # Check whether the accounts file has changed
+        with open("accounts.txt", 'rb') as file_to_check:
+            md5sum_of_accounts = md5(file_to_check.read()).hexdigest()
+            logging.info("Current md5sum: " + md5sum_of_accounts)
+            if not md5sum_of_accounts == settings_json["md5sum_of_accounts"]:
+                logging.info("Found new accounts")
+                add_accounts()
+
+
 logging.basicConfig(filename='unbotter.log', level=logging.DEBUG,
                     format='%(asctime)s |%(levelname)s| %(message)s')
 initialize()
-login_users()
