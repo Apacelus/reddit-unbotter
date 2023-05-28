7a1103442212faf2c1ec30736f95749bb692927e
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-05-29 01:58:11 +0200

    Add calendar.py stub

---
+++ calendar.py
@@ -0,0 +1,14 @@
+def remove_account(account: str) -> None:
+    """
+    Remove all entries of a given account from calendar
+    :arg account: account to be removed
+    """
+    pass
+
+
+def initialize_account(account: str) -> None:
+    """
+    Initialize a given account as per the settings in data.json
+    :arg account: account to be initialized
+    """
+    pass
